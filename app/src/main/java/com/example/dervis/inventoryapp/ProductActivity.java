680f64457af19f993bba5e48329b29a5d941b38b
mahmut <mddh111117@gmail.com>
2018-09-13 06:07:21 +0700

    finishing touches, revision 1.

--- app/src/main/java/com/example/dervis/inventoryapp/ProductActivity.java
+++ app/src/main/java/com/example/dervis/inventoryapp/ProductActivity.java
@@ -49,30 +49,27 @@ public class ProductActivity extends AppCompatActivity implements LoaderManager.
         et_supplier = findViewById(R.id.et_supplier);
         et_supplier_phone = findViewById(R.id.et_supplier_phone);
 
-        View btn_increase = findViewById(R.id.btn_increase_quantity);
-        View btn_decrease = findViewById(R.id.btn_decrease_quantity);
-
-        inputFields = new View[]{et_name, et_price, et_quantity, et_supplier, et_supplier_phone, btn_decrease, btn_increase};
+        inputFields = new View[]{et_name, et_price, et_quantity, et_supplier, et_supplier_phone, findViewById(R.id.btn_decrease_quantity), findViewById(R.id.btn_increase_quantity)};
 
 
-        View.OnTouchListener touchListener = new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                mChangeDetected = true;
-                return false;
-            }
-        };
+        setupChangeListener();
 
-        setupChangeListener(touchListener);
+        loadItemUri(getIntent().getData());
 
-        mItemUri = getIntent().getData();
+        setupQuantityButtons();
+    }
+
+    private void loadItemUri(Uri data) {
+        mItemUri = data;
 
         if (mItemUri != null) {
             getSupportLoaderManager().initLoader(PRODUCT_LOADER_ID, null, this);
         }
+    }
 
-        View[] quantityButtons = new View[]{btn_increase,
-                btn_decrease};
+    private void setupQuantityButtons() {
+        View[] quantityButtons = new View[]{findViewById(R.id.btn_increase_quantity),
+                findViewById(R.id.btn_decrease_quantity)};
 
         for (View v : quantityButtons) {
             v.setOnClickListener(new View.OnClickListener() {
@@ -101,9 +98,15 @@ public class ProductActivity extends AppCompatActivity implements LoaderManager.
         }
     }
 
-    private void setupChangeListener(View.OnTouchListener touchListener) {
+    private void setupChangeListener() {
         for (View v : inputFields) {
-            v.setOnTouchListener(touchListener);
+            v.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v1, MotionEvent event) {
+                    mChangeDetected = true;
+                    return false;
+                }
+            });
         }
     }
 
@@ -118,58 +121,86 @@ public class ProductActivity extends AppCompatActivity implements LoaderManager.
 
         switch (item.getItemId()) {
             case R.id.action_delete:
-                showConfirmDialog(new DialogInterface.OnClickListener() {
+                showConfirmDialog("Do you want to delete this Product?", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         getContentResolver().delete(mItemUri, null, null);
                         finish();
                     }
                 });
+
                 return true;
             case R.id.action_contact_supplier:
-                showConfirmDialog(new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                });
-                Intent intent = new Intent(Intent.ACTION_DIAL);
-                intent.setData(Uri.parse("tel:" + et_supplier_phone.getText().toString()));
-                startActivity(intent);
-                return true;
-            case R.id.action_save_changes:
-                ContentValues values = extractContentValues();
-
-                ContentResolver resolver = getContentResolver();
-
-                if (mItemUri != null) {
-                    resolver.update(mItemUri, values, null, null);
+                if (mChangeDetected) {
+                    showConfirmDialog("Would you like to save changes first?", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            saveChanges();
+                            contactSupplier();
+                        }
+                    });
                 } else {
-                    resolver.insert(ProductContract.ProductEntry.CONTENT_URI, values);
+                    contactSupplier();
                 }
 
+
+                return true;
+            case R.id.action_save_changes:
+
+                saveChanges();
+
                 finish();
+
                 return true;
             case android.R.id.home:
                 if (mChangeDetected) {
-                    showConfirmDialog(new DialogInterface.OnClickListener() {
+                    showConfirmDialog("Do you want to discard changes?", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
                             ProductActivity.this.finish();
                         }
                     });
+                    return true;
                 } else {
                     return false;
                 }
-                return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void showConfirmDialog(DialogInterface.OnClickListener positiveListener) {
+    private void contactSupplier() {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + et_supplier_phone.getText().toString()));
+        startActivity(intent);
+    }
+
+    private void saveChanges() {
+        ContentValues values = extractContentValues();
+
+        ContentResolver resolver = getContentResolver();
+
+        if (mItemUri != null) {
+            resolver.update(mItemUri, values, null, null);
+        } else {
+            resolver.insert(ProductContract.ProductEntry.CONTENT_URI, values);
+        }
+
+        mChangeDetected = false;
+    }
+
+    private ContentValues extractContentValues() {
+        return ProductContract.ProductEntry.CreateContentValues(
+                ((EditText) findViewById(R.id.et_product_name)).getText().toString(),
+                Integer.parseInt(((EditText) findViewById(R.id.et_price)).getText().toString()),
+                Integer.parseInt(((EditText) findViewById(R.id.et_quantity)).getText().toString()),
+                ((EditText) findViewById(R.id.et_supplier)).getText().toString(),
+                ((EditText) findViewById(R.id.et_supplier_phone)).getText().toString());
+    }
+
+    private void showConfirmDialog(String message, DialogInterface.OnClickListener positiveListener) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-        builder.setMessage("Are You Sure?");
+        builder.setMessage(message);
 
         builder.setPositiveButton("Yes", positiveListener);
 
@@ -188,7 +219,7 @@ public class ProductActivity extends AppCompatActivity implements LoaderManager.
         if (!mChangeDetected) {
             super.onBackPressed();
         } else {
-            showConfirmDialog(new DialogInterface.OnClickListener() {
+            showConfirmDialog("Are You Sure?", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     Intent upIntent = new Intent(ProductActivity.this, InventoryActivity.class);
@@ -197,15 +228,6 @@ public class ProductActivity extends AppCompatActivity implements LoaderManager.
                 }
             });
         }
-    }
-
-    private ContentValues extractContentValues() {
-        return ProductContract.ProductEntry.CreateContentValues(
-                ((EditText) findViewById(R.id.et_product_name)).getText().toString(),
-                Integer.parseInt(((EditText) findViewById(R.id.et_price)).getText().toString()),
-                Integer.parseInt(((EditText) findViewById(R.id.et_quantity)).getText().toString()),
-                ((EditText) findViewById(R.id.et_supplier)).getText().toString(),
-                ((EditText) findViewById(R.id.et_supplier_phone)).getText().toString());
     }
 
     @NonNull
